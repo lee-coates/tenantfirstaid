bdf1d89c5115a199fefd78f660bc5bd26090a065
Andrew Kostka <apkostka@gmail.com>
2025-05-16 11:00:01 -0700

    feat: change redis to DO Valkey instance feat: allow using short prompt text

--- backend/tenantfirstaid/session.py
+++ backend/tenantfirstaid/session.py
@@ -1,16 +1,24 @@
 import os
-from redis import Redis
+from valkey import Valkey
 import simplejson as json
 
 
 class TenantSession:
     def __init__(self):
-        self.redis_con = Redis(
-            host="127.0.0.1", port=6379, password=os.getenv("REDIS_PASSWORD")
-        )
+        try:
+            self.db_con = Valkey(
+                host=os.getenv("DB_HOST", "127.0.0.1"),
+                port=os.getenv("DB_PORT", 6379),
+                password=os.getenv("DB_PASSWORD"),
+                ssl=True,
+            )
+            self.db_con.ping()
+
+        except Exception as e:
+            print(e)
 
     def get(self, session_id):
-        return json.loads(self.redis_con.get(session_id) or "[]")
+        return json.loads(self.db_con.get(session_id) or "[]")
 
     def set(self, session_id, value):
-        self.redis_con.set(session_id, json.dumps(value))
+        self.db_con.set(session_id, json.dumps(value))
