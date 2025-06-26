18f52c8d81dbfb2ce84981a22de3daa5127ad286
Ka Hung Lee <lee.kahung414@gmail.com>
2025-06-25 19:25:16 -0700

    Replace reseting messages with setMessage with hard browser refresh

--- frontend/src/pages/Chat/components/MessageWindow.tsx
+++ frontend/src/pages/Chat/components/MessageWindow.tsx
@@ -30,7 +30,7 @@ export default function MessageWindow({
 
   const handleClearSession = () => {
     handleNewSession();
-    setMessages([]);
+    window.location.reload();
   };
 
   useEffect(() => {
