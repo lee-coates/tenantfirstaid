e3551152982bb4f1b3ad4e84a90e7ca071824c14
Ka Hung Lee <lee.kahung414@gmail.com>
2025-07-30 20:13:42 -0700

    Update modal placeholder for feedback

--- frontend/src/pages/Chat/components/FeedbackModal.tsx
+++ frontend/src/pages/Chat/components/FeedbackModal.tsx
@@ -22,7 +22,7 @@ export default function FeedbackModal({ messages, setOpenFeedback }: Props) {
     >
       <textarea
         className="resize-none h-[80%] w-full px-3 py-2 border-1 border-[#ddd] rounded-md box-border transition-colors duration-300 focus:outline-0 focus:border-[#4a90e2] focus:shadow-[0_0_0_2px_rgba(74,144,226,0.2)]"
-        placeholder="Please enter your feedback with regards to the chatbot here..."
+        placeholder="Please enter your feedback with regards to the chatbot here. A copy of your chat transcript will automatically be included with your response."
         onChange={(event) => setFeedback(event.target.value)}
       />
       <div className="flex gap-4">
