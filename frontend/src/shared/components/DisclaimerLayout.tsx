32a782cd1bb82ea82768504bda1830b7b049f17f
Ka Hung Lee <lee.kahung414@gmail.com>
2025-12-03 18:42:09 -0800

    Removing non-existent tailwind class for max-w-auto

--- frontend/src/shared/components/DisclaimerLayout.tsx
+++ frontend/src/shared/components/DisclaimerLayout.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function DisclaimerLayout({ isOngoing, children }: Props) {
   return (
     <div
-      className={`container mx-auto text-xs px-4 text-center ${isOngoing ? "max-w-auto my-2" : "max-w-[600px] my-4"}`}
+      className={`container mx-auto text-xs px-4 text-center ${isOngoing ? "my-2" : "max-w-[600px] my-4"}`}
     >
       <p className={`${isOngoing ? "mb-0" : "mb-2"}`}>
         <strong>Disclaimer</strong>:&nbsp;
