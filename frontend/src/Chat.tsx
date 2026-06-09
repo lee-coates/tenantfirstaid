7fc1d8203c55cb5c07d5cd80db2f9843bb4397cc
Annya <annyakd@gmail.com>
2026-06-09 16:51:23 -0500

    Add collapsible Features sidebar to Chat page
    Features column (desktop) on chat page collapses to the right using a chevron
    toggle button.

--- frontend/src/Chat.tsx
+++ frontend/src/Chat.tsx
@@ -10,6 +10,8 @@ import MobilePanel from "./shared/components/MobilePanel";
 import { Navigate, useParams } from "react-router-dom";
 import { classifyStateSegment } from "./shared/utils/jurisdiction";
 import { DEFAULT_JURISDICTION } from "./shared/constants/jurisdictions";
+import { useState } from "react";
+import ChevronRight from "./shared/components/ChevronRight";
 import clsx from "clsx";
 
 /**
@@ -44,9 +46,10 @@ function ChatView() {
   const { addMessage, messages, setMessages } = useMessages();
   const isOngoing = messages.length > 0;
   const { letterContent } = useLetterContent(messages);
+  const [featuresOpen, setFeaturesOpen] = useState(true);
 
   return (
-    <div className="min-h-full lg:h-full w-full flex flex-col lg:flex-row transition-all duration-300">
+    <div className="min-h-full lg:h-full w-full flex flex-col lg:flex-row transition-all duration-300 lg:relative lg:bg-paper-background">
       <div className="flex-1 lg:flex-none lg:my-0 w-full lg:w-3/5 flex lg:order-2">
         <MessageContainer isOngoing={isOngoing} letterContent={letterContent}>
           <div
@@ -80,21 +83,50 @@ function ChatView() {
           </div>
         </MobilePanel>
       </div>
+      <button
+        type="button"
+        onClick={() => setFeaturesOpen(!featuresOpen)}
+        className={clsx(
+          "hidden lg:flex items-center justify-center", 
+          "absolute top-4 z-10",
+          "w-6 h-6 bg-paper-background border border-gray-light rounded-l-md",
+          "transition-[right] duration-300 ease-in-out",
+          featuresOpen ? "right-[20%]" : "right-0",
+        )}
+        >
+          <span
+            className={clsx(
+              "transition-transform duration-300",
+              featuresOpen ? "rotate-0" : "rotate-180",
+            )}
+          >
+            <ChevronRight size={14} />
+          </span>
+        </button>
       <div
         className={clsx(
           "flex flex-col w-full bg-paper-background",
           "border-b lg:border-b-0 border-gray-light",
-          "lg:order-3 lg:my-0 lg:w-1/5 lg:border-l",
+          "lg:order-3 lg:my-0 lg:ml-auto",
+          "lg:transition-[width] lg:duration-300 lg:ease-in-out overflow-hidden",
+          featuresOpen ? "lg:w-1/5" : "lg:w-0",
           "[@media(max-height:800px)]:my-0 [@media(max-height:800px)]:self-stretch [@media(max-height:800px)]:overflow-hidden",
         )}
-      >
-        <MobilePanel title="Features">
-          <div className="flex flex-col flex-1 min-h-0 lg:overflow-y-auto [@media(max-height:800px)]:overflow-y-auto">
-            <FeatureSnippet />
+      >        
+        <div className={clsx(
+          "flex-1 flex flex-col min-w-0 lg:border-l border-gray-light",
+          "lg:transition-opacity lg:duration-300",
+          "lg:w-[20vw]",
+          featuresOpen ? "lg:opacity-100" : "lg:opacity-0 pointer-events-none",
+        )}>
+          <MobilePanel title="Features">
+            <div className="flex flex-col flex-1 min-h-0 lg:overflow-y-auto [@media(max-height:800px)]:overflow-y-auto">
+              <FeatureSnippet />
+            </div>
+          </MobilePanel>
+          <div className="p-4 [@media(min-width:1024px)_and_(min-height:801px)]:mt-auto">
+            <ChatDisclaimer isOngoing={isOngoing} />
           </div>
-        </MobilePanel>
-        <div className="p-4 [@media(min-width:1024px)_and_(min-height:801px)]:mt-auto">
-          <ChatDisclaimer isOngoing={isOngoing} />
         </div>
       </div>
     </div>
